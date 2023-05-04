3d6d68b8fa0bc77e11fa79092b88411d123f3d84
Mouhcinekarim <mouhcine.karim@usms.ac.ma>
2023-05-04 18:24:16 +0100

    Delete Etudiant1-Service directory

--- Etudiant1-Service/src/main/java/com/example/etudiant1service/Exceptions/EtudiantNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.etudiant1service.Exceptions;
-
-public class EtudiantNotFoundException extends Exception{
-    public EtudiantNotFoundException(String message) {
-        super(message);
-    }
-}
